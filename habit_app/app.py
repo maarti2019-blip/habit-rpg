9f313d8d54b7a7c6baab9d1c3711b5dd8a2e6d2f
maarti2019-blip <maarti2019@gmail.com>
2026-06-19 16:47:59 -0400

    Add files via upload
    updated paths to fix internal server loop.

--- habit_app/app.py
+++ habit_app/app.py
@@ -7,7 +7,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = 'rpg_accountability_secret_chain'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
